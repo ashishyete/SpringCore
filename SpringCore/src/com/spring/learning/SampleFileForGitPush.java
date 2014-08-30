6721f67ddb42b3ed552f53d0cd3395c3792d85ce
Ashish <Ashish@Ashish-PC>
2014-08-30 18:33:03 +0530

    Sample Push

---
+++ SpringCore/src/com/spring/learning/SampleFileForGitPush.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.spring.learning;
+
+/**
+ * @author Ashish
+ *
+ */
+public class SampleFileForGitPush {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample File For Git Push");
+	}
+
+}
